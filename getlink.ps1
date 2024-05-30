3fb52d82a9098637acf49f3e7e5f8bd83c50c0c3
illo.yoon <yildbs@gmail.com>
2024-05-31 08:44:53 +0900

    script update

---
+++ getlink.ps1
@@ -0,0 +1,25 @@
+Add-Type -AssemblyName System.Web
+
+$logLocation = "C:\Wuthering Waves\Wuthering Waves Game\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView\debug.log";
+
+$path = [System.Environment]::ExpandEnvironmentVariables($logLocation);
+Write-Host $path
+
+if (-Not [System.IO.File]::Exists($path)) {
+    Write-Host "Cannot find the log file! Check convene history in game!" -ForegroundColor Red
+    return
+}
+
+$logs = Get-Content -Path $path
+
+$result = select-string -path $logLocation -pattern 'aki-gm' | select-string -pattern 'gacha' | select-string -pattern 'player_id' | Where-Object { $_.Line -notmatch '.json' } | Select-Object -ExpandProperty Line  | Select-Object -First 1
+if ($result -match '(https://[^\s",]+)') {
+    $url = $matches[1]
+} else {
+    Write-Host "No URL Found" -ForegroundColor Red
+    return
+}
+Write-Output $url
+Set-Clipboard -Value $url
+
+Write-Host "Link copied to clipboard, paste it back to wwtracer" -ForegroundColor Green
